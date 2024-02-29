c62d5dbde67a71aad03de45fa6f8abd446ff971c
Aman Dangol <amandangol423@gmail.com>
2024-02-29 21:01:18 +0545

    added input field and button

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
 <body>
   <main>
     <h1>Shopping Items</h1>
+    <!-- form to submit -->
+    <section>
+      <form action="" method="post">
+        <input type="text" name = "shopping">
+        <button type="submit">add to cart</button>
+      </form>
+    </section>
 </main>
 </body>
 </html>
