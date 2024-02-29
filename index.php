a7c0bf9e0172cd693ba167c0af6cd6810110521c
Aman Dangol <amandangol423@gmail.com>
2024-02-29 21:49:01 +0545

    final commit

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+$shoppingList =$_SESSION["array"];
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +17,7 @@
     <h1>Shopping Items</h1>
     <!-- form to submit -->
     <section>
-      <form action="" method="post">
+      <form action="./add_to_cart.php" method="post">
         <input type="text" name = "shopping">
         <button type="submit">add to cart</button>
       </form>
@@ -19,8 +26,11 @@
     <section>
       <h4>Shopping list here</h4>
       <ul>
-        <li>Buy 🍺</li>
-        <li>Buy money</li>
+        <?php 
+        foreach($shoppingList as $element){
+          echo "<li>{$element}</li>";
+        }
+        ?>
       </ul>
     </section>
 </main>
